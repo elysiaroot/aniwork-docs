42788666a2e15187afa5bad1958f2a2c0356f125
HanbitGaram <HanbitGaram@users.noreply.github.com>
2023-01-19 07:38:16 +0000

    deploy: f588226619d6d109a4c6992224c5edd9d7d663a6

---
+++ assets/write-post.md.2dfde178.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o as s,b as a}from"./app.eb5baa8c.js";const u=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"write-post.md","lastUpdated":1674104098000}'),o={name:"write-post.md"},r=a("p",null,"asdasd",-1),n=[r];function c(d,p,_,i,l,m){return s(),e("div",null,n)}const h=t(o,[["render",c]]);export{u as __pageData,h as default};
